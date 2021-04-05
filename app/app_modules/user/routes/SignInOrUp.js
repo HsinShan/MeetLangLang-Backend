f2b8a898a6e65458ec460f9eb12064d5fef4b436
GengBao.Lin <lernhern@gmail.com>
2021-04-06 00:11:24 +0800

    fix(app): login api has error 'Transaction query already complete'

--- app/app_modules/user/routes/SignInOrUp.js
+++ app/app_modules/user/routes/SignInOrUp.js
@@ -12,13 +12,13 @@ class SignInOrUp {
                         await trx('user').insert({
                             email: req.body.email,
                         });
+                        list = await trx('user').where('email', req.body.email).select();
                     } catch (err) {
                         await trx.rollback();
                         throw err;
                     }
-                    await trx.commit();
                 }
-                list = await trx('user').where('email', req.body.email).select();
+                await trx.commit();
                 const payload = { id: list[0].UID };
                 const secret = 'ntusdm2021stoneocean';
                 const token = jwt.sign(payload, secret, { expiresIn: '30 days' });
