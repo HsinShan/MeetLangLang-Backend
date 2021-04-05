8ab5f9fca1d587e00704320135c8125afc86c937
GengBao.Lin <lernhern@gmail.com>
2021-04-06 00:11:31 +0800

    feat(app): reject request if required fields are missing for login api

--- app/app_modules/user/routes/SignInOrUp.js
+++ app/app_modules/user/routes/SignInOrUp.js
@@ -5,14 +5,16 @@ class SignInOrUp {
     static route() {
         return async (req, res, next) => {
             try {
+                if (!('email' in req.body)) throw Error('field `email` is missing.');
+                const { email } = req.body;
                 const trx = await AppDb.db.transaction();
-                let list = await trx('user').where('email', req.body.email).select();
+                let list = await trx('user').where('email', email).select();
                 if (list.length === 0) {
                     try {
                         await trx('user').insert({
                             email: req.body.email,
                         });
-                        list = await trx('user').where('email', req.body.email).select();
+                        list = await trx('user').where('email', email).select();
                     } catch (err) {
                         await trx.rollback();
                         throw err;
