07860a29408af61879541631b9689b31dd284035
Allen <allen010301#gmail.com>
2021-04-09 20:46:52 +0800

    feat(db): create tables for user favorite

--- db/mllapp.sql
+++ db/mllapp.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: mll-mysql:3306
--- Generation Time: Mar 30, 2021 at 12:44 PM
--- Server version: 5.6.51
--- PHP Version: 7.4.16
+-- 主機： mll-mysql:3306
+-- 產生時間： 2021 年 04 月 09 日 12:41
+-- 伺服器版本： 5.6.51
+-- PHP 版本： 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mllapp`
+-- 資料庫： `mllapp`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `example`
+-- 資料表結構 `example`
 --
 
 CREATE TABLE `example` (
@@ -36,7 +36,33 @@ CREATE TABLE `example` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- 資料表結構 `favorite_map`
+--
+
+CREATE TABLE `favorite_map` (
+  `uuid` int(8) NOT NULL,
+  `pet_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `pet_info`
+--
+
+CREATE TABLE `pet_info` (
+  `pet_id` int(10) NOT NULL,
+  `sex` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `age` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `kind` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `picture` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `user`
 --
 
 CREATE TABLE `user` (
@@ -46,37 +72,67 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Indexes for dumped tables
+-- 已傾印資料表的索引
 --
 
 --
--- Indexes for table `example`
+-- 資料表索引 `example`
 --
 ALTER TABLE `example`
   ADD PRIMARY KEY (`uuid`);
 
 --
--- Indexes for table `user`
+-- 資料表索引 `favorite_map`
+--
+ALTER TABLE `favorite_map`
+  ADD PRIMARY KEY (`uuid`,`pet_id`),
+  ADD KEY `pet_id` (`pet_id`);
+
+--
+-- 資料表索引 `pet_info`
+--
+ALTER TABLE `pet_info`
+  ADD PRIMARY KEY (`pet_id`);
+
+--
+-- 資料表索引 `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`uuid`),
   ADD UNIQUE KEY `unique_user_email` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
 --
--- AUTO_INCREMENT for table `example`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `example`
 --
 ALTER TABLE `example`
   MODIFY `uuid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `user`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `pet_info`
+--
+ALTER TABLE `pet_info`
+  MODIFY `pet_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `user`
 --
 ALTER TABLE `user`
   MODIFY `uuid` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- 已傾印資料表的限制式
+--
+
+--
+-- 資料表的限制式 `favorite_map`
+--
+ALTER TABLE `favorite_map`
+  ADD CONSTRAINT `pet_id` FOREIGN KEY (`pet_id`) REFERENCES `pet_info` (`pet_id`),
+  ADD CONSTRAINT `uuid` FOREIGN KEY (`uuid`) REFERENCES `user` (`uuid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
