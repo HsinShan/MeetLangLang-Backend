ce2364f522a35be5195c9cbbb36db3e115c330b0
Dale Chang <kyec1118@gmail.com>
2021-04-15 13:19:59 +0800

    feat(app): updated check if pet if already saved logic

--- app/app_modules/user/routes/SavePet.js
+++ app/app_modules/user/routes/SavePet.js
@@ -4,53 +4,81 @@ class SavePet {
     static route() {
         return async (req, res, next) => {
             try {
+                if (!('type' in req.body)) throw Error('type is missing');
                 if (!('uuid' in req.body)) throw Error('uuid is missing');
                 if (!('petId' in req.body)) throw Error('petId is missing');
                 const trx = await AppDb.db.transaction();
                 const uuid = req.body.uuid;
                 const petId = req.body.petId;
+                console.log(req.body.type);
 
-            //insert pet info into petInfo table
-                let petlist = await trx('PetInfo').where('petId', petId).select();
-                if (petlist.length === 0) {
+                if (req.body.type === 'check') {
                     try {
-                        await trx('PetInfo').insert({
-                            petId: petId,
-                            sex: req.body.sex,
-                            kind: req.body.kind,
-                            color: req.body.color,
-                            sterilization: req.body.sterilization,
-                            remark: req.body.remark,
-                            tel: req.body.tel,
-                            address: req.body.address,
-                            place: req.body.place,
-                            picture: req.body.picture,
-                        });
+                        //check if user has already saved pet
+                        let checklist = await trx('FavoriteMap').where({
+                            uuid: uuid,
+                            petId: petId
+                        }).select();
+                        console.log(checklist);
+                        if (checklist.length === 0) {
+                            res.status(200).json({
+                                Saved: true,
+                            })
+                        } else {
+                            res.status(200).json({
+                                Saved: false,
+                            })
+                        }
                     } catch (err) {
-                        await trx.rollback();
                         throw err;
                     }
                 }
 
-            //insert uuid & petId into FavoriteMap table
-                try {
-                    await trx('FavoriteMap').insert({
-                        uuid: uuid,
-                        petId: petId
-                    });
-                } catch (err) {
-                    await trx.rollback();
-                    throw err;
+                else {
+                    //insert pet info into petInfo table
+                    console.log(req.body.type);
+                    let petlist = await trx('PetInfo').where('petId', petId).select();
+                    if (petlist.length === 0) {
+                        try {
+                            await trx('PetInfo').insert({
+                                petId: petId,
+                                sex: req.body.sex,
+                                kind: req.body.kind,
+                                color: req.body.color,
+                                sterilization: req.body.sterilization,
+                                remark: req.body.remark,
+                                tel: req.body.tel,
+                                address: req.body.address,
+                                place: req.body.place,
+                                picture: req.body.picture,
+                            });
+                        } catch (err) {
+                            await trx.rollback();
+                            throw err;
+                        }
+                        console.log("insert favorite map");
+                        //insert uuid & petId into FavoriteMap table
+                        try {
+                            await trx('FavoriteMap').insert({
+                                uuid: uuid,
+                                petId: petId
+                            });
+                        } catch (err) {
+                            await trx.rollback();
+                            throw err;
+                        }
+                        await trx.commit();
+                        res.status(200).json({
+                            success: true,
+                        });
+                    }
                 }
-                await trx.commit();
-                res.status(200).json({
-                    success : true,
-                });
-            } catch (apiError) {
-                next(apiError);
             }
+            catch (apiError) {
+                next(apiError);
+            };
         };
     }
-}
+};
 
 module.exports = SavePet;
