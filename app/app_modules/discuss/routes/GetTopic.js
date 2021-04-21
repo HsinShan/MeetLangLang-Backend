fd3d0f142f4f22c2ff8ff6527be3976aedd6aca8
Allen <allen010301#gmail.com>
2021-04-21 17:20:44 +0800

    fix(app): modify get discuss data api for codestyle

--- app/app_modules/discuss/routes/GetTopic.js
+++ app/app_modules/discuss/routes/GetTopic.js
@@ -4,8 +4,8 @@ class GetTopic {
     static route() {
         return async (req, res, next) => {
             try {
-                //目前先將email當作者
-                const list = await AppDb.db('Message').join('User', 'Message.userId', 'User.uuid').select({key: 'Message.uuid'}, {Title: 'Message.title'}, {date: 'Message.time'}, {author: 'User.email'});
+                // 目前先將email當作者
+                const list = await AppDb.db('Message').join('User', 'Message.userId', 'User.uuid').select({ key: 'Message.uuid' }, { Title: 'Message.title' }, { date: 'Message.time' }, { author: 'User.email' });
                 res.status(200).json(list);
             } catch (apiError) {
                 next(apiError);
