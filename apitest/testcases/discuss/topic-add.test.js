e4046998775fb3a56dec1b62f2c149d2b8b43f8f
R09725064 <r09725064@ntu.edu.tw>
2021-04-26 22:56:15 +0800

    style(apitest): modify indent

--- apitest/testcases/discuss/topic-add.test.js
+++ apitest/testcases/discuss/topic-add.test.js
@@ -57,93 +57,93 @@ const testCases = (db, method, url) => () => {
                 assert.notEqual(status, 200);
             }
         });
-    it('field `title` should not be empty.', async () => {
-        const jwtToken = jwt.generateUserToken();
-        const testData = {
-            title: '',
-            content: 'testContent',
-        };
-        // Call api
-        try {
-            await axios({
-                method,
-                url,
-                headers: {
-                    token: jwtToken,
-                },
-                data: testData,
-            });
-        } catch (err) {
-            const { response } = err;
-            const { status } = response;
-            assert.notEqual(status, 200);
-        }
+        it('field `title` should not be empty.', async () => {
+            const jwtToken = jwt.generateUserToken();
+            const testData = {
+                title: '',
+                content: 'testContent',
+            };
+            // Call api
+            try {
+                await axios({
+                    method,
+                    url,
+                    headers: {
+                        token: jwtToken,
+                    },
+                    data: testData,
+                });
+            } catch (err) {
+                const { response } = err;
+                const { status } = response;
+                assert.notEqual(status, 200);
+            }
+        });
+        it('field `content` should not be empty.', async () => {
+            const jwtToken = jwt.generateUserToken();
+            const testData = {
+                title: 'testTitle',
+                content: '',
+            };
+            // Call api
+            try {
+                await axios({
+                    method,
+                    url,
+                    headers: {
+                        token: jwtToken,
+                    },
+                    data: testData,
+                });
+            } catch (err) {
+                const { response } = err;
+                const { status } = response;
+                assert.notEqual(status, 200);
+            }
+        });
+        it('field `title` is missing.', async () => {
+            const jwtToken = jwt.generateUserToken();
+            const testData = {
+                content: 'testContent',
+            };
+            // Call api
+            try {
+                await axios({
+                    method,
+                    url,
+                    headers: {
+                        token: jwtToken,
+                    },
+                    data: testData,
+                });
+            } catch (err) {
+                const { response } = err;
+                const { status } = response;
+                assert.notEqual(status, 200);
+            }
+        });
+        it('field `content` is missing.', async () => {
+            const jwtToken = jwt.generateUserToken();
+            const testData = {
+                title: 'testTitle',
+            };
+            // Call api
+            try {
+                await axios({
+                    method,
+                    url,
+                    headers: {
+                        token: jwtToken,
+                    },
+                    data: testData,
+                });
+            } catch (err) {
+                const { response } = err;
+                const { status } = response;
+                assert.notEqual(status, 200);
+            }
+        });
     });
-    it('field `content` should not be empty.', async () => {
-        const jwtToken = jwt.generateUserToken();
-        const testData = {
-            title: 'testTitle',
-            content: '',
-        };
-        // Call api
-        try {
-            await axios({
-                method,
-                url,
-                headers: {
-                    token: jwtToken,
-                },
-                data: testData,
-            });
-        } catch (err) {
-            const { response } = err;
-            const { status } = response;
-            assert.notEqual(status, 200);
-        }
-    });
-    it('field `title` is missing.', async () => {
-        const jwtToken = jwt.generateUserToken();
-        const testData = {
-            content: 'testContent',
-        };
-        // Call api
-        try {
-            await axios({
-                method,
-                url,
-                headers: {
-                    token: jwtToken,
-                },
-                data: testData,
-            });
-        } catch (err) {
-            const { response } = err;
-            const { status } = response;
-            assert.notEqual(status, 200);
-        }
-    });
-    it('field `content` is missing.', async () => {
-        const jwtToken = jwt.generateUserToken();
-        const testData = {
-            title: 'testTitle',
-        };
-        // Call api
-        try {
-            await axios({
-                method,
-                url,
-                headers: {
-                    token: jwtToken,
-                },
-                data: testData,
-            });
-        } catch (err) {
-            const { response } = err;
-            const { status } = response;
-            assert.notEqual(status, 200);
-        }
-      });
-   });
 };
 
 module.exports = testCases;
