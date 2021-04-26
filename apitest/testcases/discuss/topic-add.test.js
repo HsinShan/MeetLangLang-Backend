a61b77ca6439228419876c51c6828dd9c9d2088b
R09725064 <r09725064@ntu.edu.tw>
2021-04-26 21:51:29 +0800

    feat(apitest): remove testdata userid

--- apitest/testcases/discuss/topic-add.test.js
+++ apitest/testcases/discuss/topic-add.test.js
@@ -6,61 +6,66 @@ const testCases = (db, method, url) => () => {
     // You can define matched data here
     const shouldMatchedData = {
     };
-    // Before test case
-    before(async () => {
-        // If you want to add sample data into database
-        await db('User').insert({
-            email: 'member1@example.com',
+
+    // Positive context
+    describe('Positive Testing', () => {
+        // Before test case
+        before(async () => {
+            // If you want to add sample data into database
+            await db('User').insert({
+                email: 'member1@example.com',
+            });
         });
-    });
-    // Test case
-    it('Check normal response', async () => {
-        const jwtToken = jwt.generateUserToken();
-        // Define test data here
-        const testData = {
-            // userId: 1,
-            title: 'testTitle',
-            content: 'testContent',
-        };
-        // Call api
-        const { data } = await axios({
-            method,
-            url,
-            headers: {
-                token: jwtToken,
-            },
-            data: testData,
-        });
-        assert.isObject(data);
-        assert.include(data, shouldMatchedData);
-    });
-    it('token problem.', async () => {
-        const jwtToken = ' ';
-        const testData = {
-            userId: 1,
-            title: 'testTitle',
-            content: 'testContent',
-        };
-        // Call api
-        try {
-            await axios({
+        it('Check normal response', async () => {
+            const jwtToken = jwt.generateUserToken();
+            // Define test data here
+            const testData = {
+                // userId: 1,
+                title: 'testTitle',
+                content: 'testContent',
+            };
+            // Call api
+            const { data } = await axios({
                 method,
                 url,
                 headers: {
                     token: jwtToken,
                 },
                 data: testData,
+            }).catch((err) => {
+                console.log('err', err);
             });
-        } catch (err) {
-            const { response } = err;
-            const { status } = response;
-            assert.notEqual(status, 200);
-        }
+            assert.isObject(data);
+            assert.include(data, shouldMatchedData);
+        });
     });
+    // Negative context
+    describe('Negative Testing', () => {
+        it('token problem.', async () => {
+            const jwtToken = ' ';
+            const testData = {
+                title: 'testTitle',
+                content: 'testContent',
+            };
+            // Call api
+            try {
+                await axios({
+                    method,
+                    url,
+                    headers: {
+                        token: jwtToken,
+                    },
+                    data: testData,
+                });
+            } catch (err) {
+                const { response } = err;
+                const { status } = response;
+                assert.notEqual(status, 200);
+            }
+        });
     it('field `title` should not be empty.', async () => {
         const jwtToken = jwt.generateUserToken();
         const testData = {
-            userId: 1,
             title: '',
             content: 'testContent',
         };
@@ -106,7 +111,6 @@ const testCases = (db, method, url) => () => {
     it('field `title` is missing.', async () => {
         const jwtToken = jwt.generateUserToken();
         const testData = {
-            userId: 1,
             content: 'testContent',
         };
         // Call api
@@ -128,7 +132,6 @@ const testCases = (db, method, url) => () => {
     it('field `content` is missing.', async () => {
         const jwtToken = jwt.generateUserToken();
         const testData = {
-            userId: 1,
             title: 'testTitle',
         };
         // Call api
@@ -146,7 +149,8 @@ const testCases = (db, method, url) => () => {
             const { status } = response;
             assert.notEqual(status, 200);
         }
-    });
+      });
+   });
 };
 
 module.exports = testCases;
