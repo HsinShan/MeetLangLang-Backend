a911cc68ccc0cdd6f07f43d8747901791912fe0c
吳雨澤ALLEN010301 <allen010301@mitac.com.tw>
2021-05-03 17:22:33 +0800

    fix(app): debug of get topic api

--- app/app_modules/discuss/routes/GetTopic.js
+++ app/app_modules/discuss/routes/GetTopic.js
@@ -4,7 +4,7 @@ class GetTopic {
     static route() {
         return async (req, res, next) => {
             try {
-                const list = await AppDb.db('Message').join('User', 'Message.userId', 'User.uuid').select({ key: 'Message.uuid' }, { Title: 'Message.title' }, { date: 'Message.time' }, { author: 'User.name' });
+                const list = await AppDb.db('Message').join('User', 'Message.userId', 'User.uuid').select({ key: 'Message.uuid' }, { title: 'Message.title' }, { date: 'Message.time' }, { author: 'User.name' });
                 res.status(200).json(list);
             } catch (apiError) {
                 next(apiError);
