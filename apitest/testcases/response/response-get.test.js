abf448c219f853be86d1d495f53527bf9a01ccdd
kyec1118 <kyec1118@gmail.com>
2021-05-31 00:03:31 +0800

    test(apitest): modified code to fit standard

--- apitest/testcases/response/response-get.test.js
+++ apitest/testcases/response/response-get.test.js
@@ -2,56 +2,66 @@ const axios = require('axios');
 const { assert } = require('chai');
 
 const testCases = (db, method, url) => () => {
-    const pathVar = { mesgId: 1 };
+    // You can define matched data here
     const shouldMatchedData = {
         respId: 1,
         time: new Date().toISOString().substring(0, 10),
         author: 'testauthor',
-        content: 'testcontent1',
+        content: 'testContent1',
     };
+
+    // Before test case
     beforeEach(async () => {
         await db('User').insert({
             email: 'member1@example.com',
             name: 'testauthor',
         });
 
-        await db('Message').insert({
-            userId: '1',
-            title: 'testTitle',
-            content: 'testContent',
-        });
+        await db('Message').insert([
+            { userId: '1', title: 'testTitle1', content: 'testPost1' },
+            { userId: '1', title: 'testTitle2', content: 'testPost2' },
+        ]);
 
         await db('MesgResponse').insert([
-            { userId: 1, mesgId: 1, content: 'testcontent1' },
+            { userId: 1, mesgId: 1, content: 'testContent1' },
             { userId: 1, mesgId: 2, content: 'testcontent2' },
         ]);
     });
 
-    it('Check normal response fields', async () => {
-        const { data } = await axios({
-            method,
-            url: `${url}/${pathVar.mesgId}`,
+    // Put global vars or functions here
+    const pathVar = { mesgId: 1 };
+
+    // Positive context
+    describe('Positive Testing', () => {
+        it('Normal Case', async () => {
+            const { data } = await axios({
+                method,
+                url: `${url}/${pathVar.mesgId}`,
+            });
+            assert.isArray(data);
+            assert.include(data[0], {
+                respId: shouldMatchedData.respId,
+                author: shouldMatchedData.author,
+                content: shouldMatchedData.content,
+            });
+            assert.equal(data[0].time.substring(0, 10), shouldMatchedData.time);
         });
-        assert.isArray(data);
-        assert.include(data[0], {
-            respId: shouldMatchedData.respId,
-            author: shouldMatchedData.author,
-            content: shouldMatchedData.content,
-        });
-        assert.equal(data[0].time.substring(0, 10), shouldMatchedData.time);
     });
 
-    it('Check fetch response data error', async () => {
-        try {
-            await axios({
-                method,
-                url: `${url}/123`,
-            });
-        } catch (err) {
-            const { response } = err;
-            const { data } = response;
-            assert.equal(data.errorCode, 632);
-        }
+    // Negative context
+    describe('Negative Testing', () => {
+        it('Param mesgId is incorrect', async () => {
+            try {
+                await axios({
+                    method,
+                    url: `${url}/123`,
+                });
+            } catch (err) {
+                const { response } = err;
+                const { data } = response;
+                assert.equal(data.errorCode, 632);
+            }
+        });
     });
 };
 
